5397b30c6d4bc0721b3927daeef532d2b327e867
Milan Matic <programming4live@gmail.com>
2019-12-05 12:51:55 +0000

    Create test Component and use it in the About Page
    Here we learned that we need to create a folder src/components
    which will contain all the different files/components from our
    Gatsby App. Remember each file is a component for Gatsby.

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,7 @@
 import React from "react"
-
+import Test from '../components/test'
 export default () => 
     <div>
         Hello from Free JAMStack.training E-Commerce About page!
+        <Test></Test>
     </div>
